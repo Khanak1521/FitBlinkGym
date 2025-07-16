0232130bd54727090c2722a2aa79f328c9064a3d
Khanak <khanak.sheth21@gmail.com>
2025-07-16 16:00:51 +0530

    Initial Commit

---
+++ FitBlinkGym/FitBlinkGymApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitBlinkGymApp.swift
+//  FitBlinkGym
+//
+//  Created by Dipang Sheth on 16/07/25.
+//
+
+import SwiftUI
+
+@main
+struct FitBlinkGymApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
